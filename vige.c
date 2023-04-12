b5f123bf3421566b052543fdb25f522bfcd709d1
Nitin667 <74631112+Nitin667@users.noreply.github.com>
2023-04-12 10:19:21 +0530

    Add files via upload

---
+++ vige.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char msg[30],key[30];
+	printf("Enter the Plaintext:\n");
+	scanf("%s",msg);
+	printf("Enter the Key:\n");
+	scanf("%s",key);	
+	int msgLen = strlen(msg), keyLen = strlen(key), i, j;
+	
+    		char newKey[msgLen], encryptedMsg[msgLen], decryptedMsg[msgLen];
+	for(i = 0, j = 0; i < msgLen; ++i, ++j){
+        if(j == keyLen)
+            j = 0;
+        newKey[i] = key[j];
+    }
+    newKey[i] = '\0';
+    for(i = 0; i < msgLen; ++i)
+        encryptedMsg[i] = ((msg[i] + newKey[i]) % 26) + 'A';
+    encryptedMsg[i] = '\0';
+    for(i = 0; i < msgLen; ++i)
+        decryptedMsg[i] = (((encryptedMsg[i] - newKey[i]) + 26) % 26) + 'A';
+    decryptedMsg[i] = '\0';
+   
+    printf("\n");
+    printf("Original Message: %s", msg);
+    printf("\nKey: %s", key);
+    printf("\nNew Generated Key: %s", newKey);
+    printf("\nEncrypted Message: %s", encryptedMsg);
+    printf("\nDecrypted Message: %s", decryptedMsg);
+return 0;
+}
